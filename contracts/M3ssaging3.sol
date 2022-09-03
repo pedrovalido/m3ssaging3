d57353d23a292d6e197f2ec02e7d409e9dc263bb
pedrovalido <pedro.validex@gmail.com>
2022-09-03 17:53:40 +0100

    create sendMessage function

--- contracts/M3ssaging3.sol
+++ contracts/M3ssaging3.sol
@@ -5,15 +5,25 @@ import "hardhat/console.sol";
 
 contract M3ssaging3 {
     
-    struct user {
+    struct User {
         string name;
-        mapping(string => address) contacts;   
+        mapping(string => address) contacts;
+        mapping(address=>Message[]) chat;
+        mapping(address=>uint256) counterMessages;
     }
 
-    mapping(address=>user) users;
+    struct Message {
+        string message;
+        address sender;
+        address receiver;
+    }
 
+    mapping(address=>User) users;
+
+    //new contact added "pedro - 0xas23fj2..."
     event NewContact(string name, address contact);
-
+    //message sent to 0x1b42ub41..."
+    event MessageSent(address receiver);
 
     constructor() {
         
@@ -23,6 +33,20 @@ contract M3ssaging3 {
     function addContact (address contact, string memory name) public {
         users[msg.sender].contacts[name] = contact;
         emit NewContact(name, contact);
+    }
+
+    function sendMessage (address _receiver, string memory _message) public {
+        //create message
+        Message memory message = Message(_message, msg.sender, _receiver);
+        
+        User storage sender = users[msg.sender];
+        User storage receiver = users[_receiver];
+        //store in sender
+        sender.chat[_receiver][sender.counterMessages[_receiver]++] = message;
+        //store in receiver
+        receiver.chat[msg.sender][receiver.counterMessages[msg.sender]++] = message;
+        
+        emit MessageSent(_receiver);
     }
 
 }
